007cf3e9f9812d847d613613f5974d4f9d0c915b
zabtani <zabtani@gmail.com>
2021-08-18 08:45:32 +0300

    projects update

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -8,15 +8,31 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import classes from './Project.module.css';
-
+import { Github, Reactjs, Html, Js, Npm, Vscode, Css } from '../../Components';
 export default function MediaCard(props) {
-  console.log(props.img);
+  const projectTechsSvgs = props.techs.map((tech) => {
+    switch (tech) {
+      case 'react':
+        return <Reactjs />;
+      case 'html':
+        return <Html />;
+      case 'javascript':
+        return <Js />;
+      case 'css':
+        return <Css />;
+
+      default:
+        throw new Error(`found no techs svgs for ${props.name} `);
+    }
+  });
   return (
     <Card className={classes.project}>
       <CardActionArea>
         <a href={props.demoUrl}>
           <Typography className={classes.title} variant="h5" component="h2">
             {props.name}
+
+            <div className={classes.svgs}>{projectTechsSvgs}</div>
           </Typography>
           <CardMedia
             component="img"
@@ -33,7 +49,7 @@ export default function MediaCard(props) {
       </CardContent>
       <CardActions className={classes.actions}>
         <Button size="small" color="primary">
-          view source
+          <a href={props.sourceUrl}> view code</a>
         </Button>
 
         {props.demoUrl && (
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -6,37 +6,67 @@ import ipcheckJPG from './ipcheck.jpg';
 import protfolioJPG from './protfolio.jpg';
 import Project from './Project';
 const Projects = (props) => {
+  const react = <b>React</b>;
+  const plainJs = <b>Plain JavaScript</b>;
   const projects = [
     {
       name: 'Noter App',
-      description:
-        'notes app build with React. allow to add dynamic labels and notes base on them. includes active/completed note list distribution, with a filter feature for each list.',
+      description: (
+        <span>
+          Notes app build with {react}. allow to add dynamic labels and notes
+          base on them. includes active/completed note list distribution, with a
+          filter feature for each list.
+        </span>
+      ),
       demoUrl: 'https://zabtani.github.io/Noter/',
+      sourceUrl: 'https://github.com/zabtani/Noter',
       img: noterJPG,
+      techs: ['react', 'html', 'css'],
+    },
+    {
+      name: 'IP check',
+      description: (
+        <span>
+          Check IP addresses app build with {react}. gives information base on
+          provided input or base on client IP (when first enter if via desktop).
+          works with several API's and provides information on the IP location
+          together with a map.
+        </span>
+      ),
+      demoUrl: 'https://zabtani.github.io/IP-check/',
+      sourceUrl: 'https://github.com/zabtani/IP-check',
+      img: ipcheckJPG,
+      techs: ['react', 'html', 'css'],
     },
     {
       name: 'Shop App',
-      description: 'bla bla bla',
+      description: (
+        <span>
+          Shop app build with {plainJs}. this ecommerce shop generates dynamic
+          catigories like prices,discounts,geners and brands base on the data
+          provided to the app.
+        </span>
+      ),
       demoUrl: 'https://zabtani.github.io/shop/',
+      sourceUrl: 'https://github.com/zabtani/shop',
       img: shopJPG,
+      techs: ['javascript', 'html', 'css'],
     },
     {
       name: 'Colors game',
       description: 'bla bla bla',
       demoUrl: 'https://zabtani.github.io/Colors-game/',
+      sourceUrl: 'https://github.com/zabtani/Colors-game',
       img: colorsGameJPG,
-    },
-    {
-      name: 'IP check',
-      description: 'bla bla bla',
-      demoUrl: 'https://zabtani.github.io/IP-check/',
-      img: ipcheckJPG,
+      techs: ['javascript', 'html', 'css'],
     },
     {
       name: 'Protfolio',
       description: 'bla bla bla',
       demoUrl: false,
+      sourceUrl: 'https://github.com/zabtani/Protfolio',
       img: protfolioJPG,
+      techs: ['react', 'html', 'css'],
     },
   ];
 
@@ -46,7 +76,7 @@ const Projects = (props) => {
       className={`${props.className} ${classes.projects}`}
     >
       <h2>My Projects</h2>
-      <p>this is my projects.</p>
+      <p>projects that i build in order to practice</p>
       <div className={classes.projectsContainer}>
         {projects.map((project) => {
           return <Project {...project} />;
