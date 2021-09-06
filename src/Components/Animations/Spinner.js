b223ad5567f1d3755f198da082e73f9c25eb61a9
zabtani <zabtani@gmail.com>
2021-09-06 15:33:36 +0300

    spinner

--- src/Components/Animations/Spinner.js
+++ src/Components/Animations/Spinner.js
@@ -1,7 +1,15 @@
 import Loader from 'react-loader-spinner';
 import React from 'react';
 const Spinner = () => {
-  return <Loader type="Oval" color="#00BFFF" height={10} width={20} />;
+  return (
+    <Loader
+      style={{ backgroundColor: 'transparent', padding: '1%' }}
+      type="Oval"
+      color="#00BFFF"
+      height={10}
+      width={40}
+    />
+  );
 };
 
 export default Spinner;
