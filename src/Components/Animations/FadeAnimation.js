88adb20e15ebb494dd59e124ba84cfd0c0000381
zabtani <zabtani@gmail.com>
2021-09-20 15:17:06 +0300

    mobile menu bug fixed

--- src/Components/Animations/FadeAnimation.js
+++ src/Components/Animations/FadeAnimation.js
@@ -1,7 +1,7 @@
 import Fade from '@material-ui/core/Fade/';
 const FadeAnimation = (props) => {
   return (
-    <Fade in={props.in === false ? false : true} timeout={{ enter: 1750 }}>
+    <Fade in={props.in === false ? false : true} timeout={{ enter: 650 }}>
       {props.children}
     </Fade>
   );
